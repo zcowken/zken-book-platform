fc0ec27f2ea3434cd7216168d9d7d7322ee93ffa
zcowken <2601436406@qq.com>
2024-06-22 20:01:47 +0800

    去除文件

--- book-platform-server/src/main/java/com/books/BookPlatformServerApplication.java
+++ book-platform-server/src/main/java/com/books/BookPlatformServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
+
 @EnableSwagger2WebMvc
 @SpringBootApplication
 public class BookPlatformServerApplication {
