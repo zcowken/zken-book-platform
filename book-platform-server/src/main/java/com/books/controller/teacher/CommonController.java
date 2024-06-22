88e44eebc070efcd39389aec3518d4e1b54ca955
zcowken <2601436406@qq.com>
2024-06-22 16:17:36 +0800

    修改：修复公共上传稿件的文件逻辑

--- book-platform-server/src/main/java/com/books/controller/teacher/CommonController.java
+++ book-platform-server/src/main/java/com/books/controller/teacher/CommonController.java
@@ -68,4 +68,45 @@ public class CommonController {
             return Result.error("file is null");
         }
     }
+
+    @PostMapping("/uploadContribute")
+    public Result<String> uploadContribute(MultipartFile file) {
+        if (file != null) {
+            // 1. 指定文件信息
+            String uploadFileName = file.getOriginalFilename();
+            String uploadFileNameSuffix = "";
+            if (uploadFileName != null) {
+                int suffixPosition = uploadFileName.lastIndexOf('.');
+                if (suffixPosition != -1) {
+                    uploadFileNameSuffix = uploadFileName.substring(suffixPosition);
+                }
+            }
+            // 获取加入bucket的文件新的名称
+            String objectName = "contribute/" + (UUID.randomUUID().toString()) + uploadFileNameSuffix;
+
+            //log信息
+            System.out.println("准备上传文件");
+            String contentType = file.getContentType();
+            System.out.println("文件OriginName：" + uploadFileName);
+            System.out.println("文件ContentType：" + contentType);
+
+            //tru方式文件上传
+            try {
+                minIOUtil.uploadByBytes(file, objectName);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            } catch (InvalidKeyException e) {
+                throw new RuntimeException(e);
+            }
+
+            String fileURL = minIOUtil.publicShare(objectName);
+            log.info("请求公共文件，输出public访问的链接:{}", fileURL);
+            return Result.success(fileURL);
+        } else {
+            System.out.println("file is null");
+            return Result.error("file is null");
+        }
+    }
 }
