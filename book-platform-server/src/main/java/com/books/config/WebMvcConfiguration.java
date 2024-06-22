ac844be782aad5cbca9a755be0d355e0a55e1149
zcowken <2601436406@qq.com>
2024-06-22 16:22:56 +0800

    添加：添加对微信登录的拦截器

--- book-platform-server/src/main/java/com/books/config/WebMvcConfiguration.java
+++ book-platform-server/src/main/java/com/books/config/WebMvcConfiguration.java
@@ -53,7 +53,8 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         registry.addInterceptor(jwtTokenUserInterceptor)
                 .addPathPatterns("/user/**")
                 .excludePathPatterns("/user/user/login")
-                .excludePathPatterns("/user/user/register");
+                .excludePathPatterns("/user/user/register")
+                .excludePathPatterns("/user/user/wxlogin");
 
         registry.addInterceptor(jwtTokenTeacherInterceptor)
                 .addPathPatterns("/teacher/**")
